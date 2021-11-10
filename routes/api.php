e1a973904492a7000a3c53135025e49f68da4d8c
Darling Sánchez <darlingprogramador47@gmail.com>
2021-11-09 18:08:01 -0600

    Api rest en laravel 8

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Api\LoginController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use \App\Http\Controllers\Api\V1\PostController as PostV1;
+use \App\Http\Controllers\Api\V2\PostController as PostV2;
+
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['index','show','destroy'])
+    ->middleware('auth:sanctum');
+
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['index','show','destroy'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [
+    LoginController::class,
+    'login'
+    ]
+);
+
+//PARA USAE UN SOLO MIDDLEWARE EN VARIAS RUTAS
+/*Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('v1/posts', PostV1::class);
+    Route::apiResource('v2/posts', PostV2::class);
+});*/
